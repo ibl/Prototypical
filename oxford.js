2a116fe27e9e3424874aeb7ade768b13a7b14600
David Robbins <robbins.david@gmail.com>
2014-12-11 07:06:01 -0600

    Added ability to initialize objects using URL's

---
+++ oxford.js
@@ -0,0 +1,5 @@
+({
+    "prototype": "https://48067914bb3d7935906839bc04226b8b5f55d44b-www.googledrive.com/host/0Bzu4cytkv4B8aXZ0UUpiXzkzclE/boxer.js",
+    "color": "white",
+    "name": "oxford"
+})
--- prototypical.js
+++ prototypical.js
@@ -1,6 +1,7 @@
 console.log("Hello from Prototypical");
 
 function Thing(initialStuff) {
+    if (typeof initialStuff !== "object") initialStuff = prototypical.quickGet(initialStuff)
     var keys = Object.keys(initialStuff);
     for (var i = keys.length - 1; i >= 0; i--) {
         this[keys[i]] = initialStuff[keys[i]]
@@ -30,6 +31,9 @@ window.prototypical = {
     demo2: function () {
         var dog = { name: "Monty", prototype: "https://48067914bb3d7935906839bc04226b8b5f55d44b-www.googledrive.com/host/0Bzu4cytkv4B8aXZ0UUpiXzkzclE/corgi.js" };
         return new Thing(dog);
+    },
+    demo3: function () {
+        return new Thing("https://48067914bb3d7935906839bc04226b8b5f55d44b-www.googledrive.com/host/0Bzu4cytkv4B8aXZ0UUpiXzkzclE/oxford.js");
     },
     quickGet: function quickGet(url) {
         var req = new XMLHttpRequest(), resp;
